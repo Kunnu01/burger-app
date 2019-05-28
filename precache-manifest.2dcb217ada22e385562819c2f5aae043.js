ccfa751510c74b6b6ae4662951e0ea67fc808669
kunal <kunalbhatia2409@gmail.com>
2019-05-28 12:27:03 +0530

    Updates

---
+++ precache-manifest.2dcb217ada22e385562819c2f5aae043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ad309a9ba8f52bb032f2c80d881f77",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "26b10ea120db6e3a2bcb",
+    "url": "/burger-app/static/css/main.17ad4b1d.chunk.css"
+  },
+  {
+    "revision": "9532318361c41b3c34b5",
+    "url": "/burger-app/static/js/2.9d10da3c.chunk.js"
+  },
+  {
+    "revision": "26b10ea120db6e3a2bcb",
+    "url": "/burger-app/static/js/main.46cd3bda.chunk.js"
+  },
+  {
+    "revision": "8ee138cb49c5963e2e75",
+    "url": "/burger-app/static/js/runtime~main.20ac298b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/logo.b8503d26.png"
+  }
+]);
